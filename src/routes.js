cd5f9768c5ada8a7877826b3210af79ae26fa750
Marcus Vinicius <vmvini@hotmail.com>
2017-10-10 18:25:25 -0300

    passing req to user handler callback

--- src/routes.js
+++ src/routes.js
@@ -3,49 +3,45 @@ const api = Router();
 const twitter = require('./twitterApi/apiFacade.js');
 const httpUtils = require('./httpUtils.js');
 
-module.exports = function (consumerKey, consumerSecret, oauth_callback, userHandler, callback_render){
+module.exports = function(consumerKey, consumerSecret, oauth_callback, userHandler, callback_render) {
     twitter.init(consumerKey, consumerSecret, oauth_callback);
     registerRoutes(userHandler, callback_render);
     return api;
 };
 
 
-function registerRoutes(userHandler, callback_render){
-    api.get('/twitter/signin', function(req, res, next){
-        
+function registerRoutes(userHandler, callback_render) {
+    api.get('/twitter/signin', function(req, res, next) {
+
         twitter
-        .getRequestToken()
-        .then(token=> httpUtils.sendSuccessResponse(res, { request_token: token})
-            , err=> httpUtils.serverError(res, err));
+            .getRequestToken()
+            .then(token => httpUtils.sendSuccessResponse(res, { request_token: token }), err => httpUtils.serverError(res, err));
 
     });
 
-    api.get('/twitter/callback', function(req, res, next){
+    api.get('/twitter/callback', function(req, res, next) {
 
         twitter
-        .getAccessToken(req.query.oauth_token, req.query.oauth_verifier)
-        .then(token=>{
-            twitter
-            .getUserDetails(token)
-            .then(user => {
-                if(userHandler){
-                    token.request_token = req.query.oauth_token;
-                    userHandler(user, token);
-                }
-                if(callback_render){
-                    res.status(200);
-                    callback_render(res);
-                }
-                else{
-                    httpUtils.sendSuccessResponse(res, {});
-                }
-                
+            .getAccessToken(req.query.oauth_token, req.query.oauth_verifier)
+            .then(token => {
+                twitter
+                    .getUserDetails(token)
+                    .then(user => {
+                        if (userHandler) {
+                            token.request_token = req.query.oauth_token;
+                            userHandler(user, token, req);
+                        }
+                        if (callback_render) {
+                            res.status(200);
+                            callback_render(res);
+                        } else {
+                            httpUtils.sendSuccessResponse(res, {});
+                        }
+
+
+                    }, err => httpUtils.serverError(res, err));
 
             }, err => httpUtils.serverError(res, err));
 
-        }, err => httpUtils.serverError(res, err));
-
     });
 }
-
-
